89cd25524ba47c395c45699d2a067c0786b9f85c
Niraj Patel <786nirajpatel@gmail.com>
2020-11-29 22:40:56 -0500

    finished election 2010 frontend page and started backend

--- __init__.py
+++ __init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/datasets')
+def dataset():
+    return render_template("opendata.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
